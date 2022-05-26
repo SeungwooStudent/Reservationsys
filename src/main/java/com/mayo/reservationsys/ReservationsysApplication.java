799ebffaa3f01ddf4f3c8ef383c898abe37a0371
ggoma0626 <zxcqwe36@naver.com>
2022-05-26 15:24:51 +0900

    start

---
+++ src/main/java/com/mayo/reservationsys/ReservationsysApplication.java
@@ -0,0 +1,13 @@
+package com.mayo.reservationsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationsysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mayo/reservationsys/ReservationsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayo.reservationsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
